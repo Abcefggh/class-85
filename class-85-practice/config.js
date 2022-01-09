d798112a4eaa1a72dfa1b6d36dafdcfe7860fc05
Abcefggh <75517235+Abcefggh@users.noreply.github.com>
2022-01-09 12:34:15 +0530

    Add files via upload

---
+++ class-85-practice/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDafBkeAwa6peI9A9tRX4xFtz1G2s6MnTg",
+  authDomain: "storytellingapp-74826.firebaseapp.com",
+  projectId: "storytellingapp-74826",
+  storageBucket: "storytellingapp-74826.appspot.com",
+  messagingSenderId: "744898101093",
+  appId: "1:744898101093:web:5d59624bf4d7ad3bfb2b8c",
+  measurementId: "G-GGLQZ6MX8L"
+};
